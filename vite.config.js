b3bb3cca0856e0ba0da5668f414aa187224bb0fb
Jason <kiralorenzo001@gmail.com>
2026-07-12 15:50:11 +0100

    Designing auth forms

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,6 @@ export default defineConfig({
             input: ['resources/css/reset.css', 'resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
                 bunny('Poppins', {
                     weights: [400, 500, 600, 700],
                 }),
